0648147b1d368b0a0ccae61782d6d66c14c77b3b
youichi yamazaki <denncoru@yahoo.co.jp>
2019-12-04 12:13:23 +0900

    carrierwaveの設定を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,13 +18,10 @@ CarrierWave.configure do |config|
 
     case Rails.env
     when 'production'
-        config.fog_directory = '本番環境用のバケット名'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+        config.fog_directory = 'insta-production'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/insta-production'
     when 'development'
-        config.fog_directory = '開発環境用のバケット名'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
-    when 'test'
-        config.fog_directory = 'テスト環境用のバケット名'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名'
+        config.fog_directory = 'insta-development'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/insta-development'
     end
 end
